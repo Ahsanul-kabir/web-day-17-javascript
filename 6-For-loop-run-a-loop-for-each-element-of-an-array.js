866def39938bd9c5ad5dfad1316478907e2747d1
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-18 20:05:25 +0600

    6-For-loop-run-a-loop-for-each-element-of-an-array added

---
+++ 6-For-loop-run-a-loop-for-each-element-of-an-array.js
@@ -0,0 +1,10 @@
+// for loop syntax
+// for (var i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+var nums = [10, 20, 30, 40, 50, 60];
+for (i = 0; i < nums.length; i++) {
+    element = nums[i]
+    console.log(element);
+}
