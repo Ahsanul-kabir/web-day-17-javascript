9ee2df7676423375c1d7f44afa71e149b2502f87
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-18 11:52:03 +0600

    var position = friendsAge.indexOf(14); // chack position if not present result is -1. added

---
+++ 2. Array, index, set by index, indexOf.js
@@ -0,0 +1,18 @@
+var friendsAge = [15, 17, 14, 16]; // value insert
+
+var sonaliAge = friendsAge[2]; // search specific value search
+
+friendsAge[1] = 21; // update array
+
+console.log(sonaliAge); //specific one person age
+
+console.log(friendsAge); // output of hole array
+
+
+var position = friendsAge.indexOf(14); // chack position if not present result is -1.
+
+console.log(position);
+
+var position = friendsAge.indexOf(141); // chack position if not present result is -1.
+
+console.log(position);
