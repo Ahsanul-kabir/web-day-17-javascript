b6c29ff0abad9992d4b7cc0260bc5af9ee32a468
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-18 22:27:50 +0600

    9-Function-parameter-multiple-parameter-function-return-1 & 2 added

---
+++ 9-Function-parameter-multiple-parameter-function-return-1.js
@@ -0,0 +1,8 @@
+function doubleIT(num) {
+    var result = num * 2;
+    console.log(result);
+}
+
+doubleIT(5);
+
+doubleIT(10);
---
+++ 9-Function-parameter-multiple-parameter-function-return-2.js
@@ -0,0 +1,11 @@
+function doubleIT(num) {
+    var result = num * 2;
+    return result;
+}
+
+var first = doubleIT(5);
+var secound = doubleIT(10);
+
+total = first + secound;
+console.log(first, secound);
+console.log(total);
