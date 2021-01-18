6112a66ba8c92404159468d2b6042ff8f5d19fcc
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-18 21:22:32 +0600

    8-Function-call-function added

---
+++ 8-Function-call-function.js
@@ -0,0 +1,11 @@
+function sayLoveYou() {
+    console.log('Jaan o baby');
+    console.log('Billy baby... koi');
+}
+
+sayLoveYou();
+var date = 14;
+var place = "Restaurent";
+sayLoveYou();
+var hangoutPlace = "TSC";
+sayLoveYou();
