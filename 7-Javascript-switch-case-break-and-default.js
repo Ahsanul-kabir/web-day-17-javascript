f334806d673f52bb407dc8903b11e957f53be903
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-18 21:00:25 +0600

    7-Javascript-switch-case-break-and-default added

---
+++ 7-Javascript-switch-case-break-and-default.js
@@ -0,0 +1,16 @@
+number = 100;
+
+switch (number) {
+    case 1000:
+        console.log(' I am 1000');
+        break;
+    case 100:
+        console.log(' I am 100');
+        break;
+    case 50:
+    case 40:
+        console.log(' I am ');
+        break;
+    default:
+        console.log('Its nothing')
+}
