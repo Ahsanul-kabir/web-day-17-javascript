720a1a674a873a6e71c05ecf2d2a5835192a0a0b
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-18 13:03:48 +0600

    3-Array-advanced-push-pop-array-length updated

--- 3-Array-advanced-push-pop-array-length.js
+++ 3-Array-advanced-push-pop-array-length.js
@@ -2,6 +2,18 @@ var friendsAge = [15, 17, 14, 16];
 
 console.log(friendsAge);
 
-friendsAge.push(20);
+friendsAge.push(20); // last e new element dukano
 friendsAge.push(30);
 console.log(friendsAge);
+
+console.log(friendsAge.length); // check length
+
+
+friendsAge.pop()
+console.log(friendsAge) // last theke element remove kora
+
+friendsAge.shift(); // remove element from the begining
+console.log(friendsAge)
+
+friendsAge.unshift(12); // add element from the begining
+console.log(friendsAge)
